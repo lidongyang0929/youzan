4096978fd8d53b5a1f29c8a38ac6ca52ac0998ee
lidongyang <15650785172@163.com>
2019-03-31 17:56:47 +0800

    添加foot组件

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -3,20 +3,48 @@ import './index.css'
 import Vue from 'vue'
 import url from 'js/api'
 import axios from 'axios'
+import { InfiniteScroll } from 'mint-ui'
+import  foot from 'component/foot'
+
+Vue.use(InfiniteScroll);
 
 
 new Vue({
     el:'#app',
     data:{
         lists:null,
+        pageNum: 1,
+        pageSize:6,
+        loading: false,    
+        allLoaded: false
     },
     created(){
-        axios.get(url.hotLists,{
-            pageNum:1,
-            pageSize: 5
-        }).then((res)=>{
-          this.lists = res.data.lists
+      this.getLists() 
+    },
+    methods:{
+        getLists(){
+            if(this.allLoaded) return
+            this.loading = true     
+            axios.get(url.hotLists,{
+                pageNum:this.pageNum,
+                pageSize: this.pageSize
+            }).then((res)=>{
+              let curLists = res.data.lists
+              if(curLists.length < this.pageSize){ this.allLoaded = true}
+              if(this.lists){
+              this.lists = this.lists.concat(curLists)
+            }else{
+                this.lists = res.data.lists
+            }
+            this.loading = false 
+            this.pageNum ++
         })
+            
+        } 
+    },
+    components:{
+        foot
     }
+   
 
 })
