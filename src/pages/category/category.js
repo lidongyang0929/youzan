3821e40e9c5f30ad6669264b0e8ab0ce6760e991
lidongyang <15650785172@163.com>
2019-04-01 19:38:16 +0800

    添加分页面

---
+++ src/pages/category/category.js
@@ -0,0 +1,50 @@
+import './category.css'
+import 'css/common.css'
+import axios from 'axios'
+import Vue from 'vue'
+import url from 'js/api'
+import foot from 'component/foot'
+
+new Vue({
+    el:'#app',
+    data:{
+    topLists:null,
+    goods:null,
+    subData:null,
+    topIndex:0,
+    shops: null,
+    keywords: null
+    },
+    methods:{
+        getTopLists(){
+            axios.get(url.topList).then(res =>{
+               this.topLists = res.data.lists
+            })
+        },
+        getSubLists(index){
+            this.topIndex = index
+            if(index === 0){
+                this.getRankLists()
+            }else{
+            axios.get(url.subLists).then(res=>{
+                this.subData = res.data.data
+                 
+            })}
+        },
+        getRankLists(){
+            axios.get(url.rankLists).then(res=>{
+                this.goods= res.data.data.hotGoods
+                this.shops = res.data.data.hotShops
+                this.keywords = res.data.data.hotKeywords
+            })
+        }
+        
+    },
+    created(){
+        this.getTopLists()
+        this.getSubLists(0)
+    },
+    components:{
+        foot
+    }
+})
